bb2b01f482e01e79cd00b1cd168a2c9d9a2af501
doroh70 <oghenedavid123@gmail.com>
2024-05-11 01:35:22 -0600

    p0partB first attempt

--- src/github.com/cmu440/p0partB/squarer_test.go
+++ src/github.com/cmu440/p0partB/squarer_test.go
@@ -31,6 +31,52 @@ func TestBasicCorrectness(t *testing.T) {
 	}
 }
 
-func TestYourFirstGoTest(t *testing.T) {
-	// TODO: Write a test here.
+func TestConcurrentInput(t *testing.T) {
+	fmt.Println("Running TestConcurrentInput.")
+	inputChan := make(chan int, 10)
+	squarer := SquarerImpl{}
+	outputChan := squarer.Initialize(inputChan)
+	go func() {
+		for i := 0; i < 5; i++ {
+			inputChan <- i
+		}
+	}()
+	go func() {
+		for i := 5; i < 10; i++ {
+			inputChan <- i
+		}
+	}()
+	//Give routines time to finish
+	time.Sleep(time.Duration(50) * time.Millisecond)
+	close(inputChan)
+	received := make(map[int]bool)
+	for i := 0; i < 10; i++ {
+		result := <-outputChan
+		received[result] = true
+	}
+	for i := 0; i < 10; i++ {
+		if _, ok := received[i*i]; !ok {
+			t.Errorf("Missing square for %d", i)
+		}
+	}
+}
+
+func TestClose(t *testing.T) {
+	fmt.Println("Running TestClose.")
+	inputChan := make(chan int)
+	sq := SquarerImpl{}
+	squaresChan := sq.Initialize(inputChan)
+	//Test that output channel is closed after Close() method is called
+	sq.Close()
+	timeoutChan := time.After(time.Duration(timeoutMillis) * time.Millisecond)
+	//Let sender close input Chan
+	close(inputChan)
+	select {
+	case _, ok2 := <-squaresChan:
+		if ok2 {
+			t.Error("Squares output channel not closed.")
+		}
+	case <-timeoutChan:
+		t.Error("TestClose timed out.")
+	}
 }
